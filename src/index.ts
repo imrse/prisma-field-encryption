a84a928ded359347a38ada04a449762216be6ca7
Kristian Kvarnsbacke <kristian@kmja.com>
2024-06-03 13:52:59 +0200

    feat: Export KeysConfiguration type for external access

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { fieldEncryptionExtension } from './extension' // Prisma >= 4.7.0
 export { fieldEncryptionMiddleware } from './middleware' // Prisma >= 3.8
+export type { KeysConfiguration } from './encryption'
